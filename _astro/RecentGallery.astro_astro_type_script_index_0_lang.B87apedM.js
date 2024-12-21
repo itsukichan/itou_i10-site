bd9172e6caa8174c7a063364b1a6b18d9575dfb8
itsukichan <itsukichan@users.noreply.github.com>
2024-12-21 01:39:38 +0000

    deploy: 9cdabf7e73fef1347dfc92f65fb0ed180f4ef8bd

---
+++ _astro/RecentGallery.astro_astro_type_script_index_0_lang.B87apedM.js
@@ -0,0 +1,1 @@
+import{g as e}from"./simpleParallaxVanilla.es.C6sHBNCS.js";const l=document.querySelectorAll(".js-parallax-recent-gallery");new e(l,{delay:1,scale:1.5,transition:"cubic-bezier(0,0,0,1)"});
