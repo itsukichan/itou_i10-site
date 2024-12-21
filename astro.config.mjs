9cdabf7e73fef1347dfc92f65fb0ed180f4ef8bd
itsukichan <kou05ituki09design@gmail.com>
2024-12-21 10:39:14 +0900

    add: .nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://あなたのユーザー名.github.io",
-  base: "/itou_i10-site", // スラッシュの重複を削除
+  site: "https://itsukichan.github.io",
+  base: "/itou_i10-site",
+  outDir: "./dist",
 });
