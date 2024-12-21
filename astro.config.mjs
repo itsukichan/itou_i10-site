7170ee78565664a3eadfef03e5d5a8028d485194
itsukichan <kou05ituki09design@gmail.com>
2024-12-21 10:53:09 +0900

    fix: coonfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://itsukichan.github.io",
-  base: "/itou_i10-site",
+  base: "/itou_i10-site/",
   outDir: "./dist",
 });
